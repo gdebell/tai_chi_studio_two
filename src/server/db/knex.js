34a3d0ea71ca394408bb7a0f02d8812796024141
Akiko Okabe <aki0u0@gmail.com>
2016-10-05 11:59:04 -0600

    seed

--- src/server/db/knex.js
+++ src/server/db/knex.js
@@ -2,4 +2,5 @@ const environment = process.env.NODE_ENV;
 const config = require('../../../knexfile.js')[environment];
 const knex = require('knex')(config);
 knex.migrate.latest();
+knex.seed.run();
 module.exports = knex;
